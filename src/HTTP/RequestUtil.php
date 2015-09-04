ad188559c6ad65c4f59d19c0064a5f5f64e94bd0
solvire <stevenjscott@gmail.com>
2015-09-03 22:45:52 -0700

    removed unnecessary var

--- src/HTTP/RequestUtil.php
+++ src/HTTP/RequestUtil.php
@@ -23,21 +23,19 @@ class RequestUtil
      */
     public static function ip()
     {
-        $ipaddress = '';
         if (isset($_SERVER['HTTP_CLIENT_IP']) && $_SERVER['HTTP_CLIENT_IP'])
-            $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+            return $_SERVER['HTTP_CLIENT_IP'];
         elseif (isset($_SERVER['HTTP_X_FORWARDED_FOR']) && $_SERVER['HTTP_X_FORWARDED_FOR'])
-            $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+            return $_SERVER['HTTP_X_FORWARDED_FOR'];
         elseif (isset($_SERVER['HTTP_X_FORWARDED']) && $_SERVER['HTTP_X_FORWARDED'])
-            $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+            return $_SERVER['HTTP_X_FORWARDED'];
         elseif (isset($_SERVER['HTTP_FORWARDED_FOR']) && $_SERVER['HTTP_FORWARDED_FOR'])
-            $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+            return $_SERVER['HTTP_FORWARDED_FOR'];
         elseif (isset($_SERVER['HTTP_FORWARDED']) && $_SERVER['HTTP_FORWARDED'])
-            $ipaddress = $_SERVER['HTTP_FORWARDED'];
+            return $_SERVER['HTTP_FORWARDED'];
         elseif (isset($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'])
-            $ipaddress = $_SERVER['REMOTE_ADDR'];
+            return $_SERVER['REMOTE_ADDR'];
         else
-            $ipaddress = 'UNKNOWN';
-        return $ipaddress;
+            return 'UNKNOWN';
     }
 }
